c63db9ebd10b7813e2f0f8937fa4f379f605b545
shevchuk-dima <dima.shevtchuk@gmail.com>
2022-11-02 22:10:57 +0200

    pagination

--- src/modules/Main/Main.jsx
+++ src/modules/Main/Main.jsx
@@ -16,12 +16,13 @@ function Main(props) {
   const [curCard, setCurCard] = useState({});
   const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [prevPage, setPrevPage] = useState(0);
+  const [nextPage, setNextPage] = useState(10);
   let arrPages = [];
 
-  for (let i = 1; i <= totalPages; i++){
+  for (let i = 1; i <= totalPages; i++) {
     arrPages.push(i);
   }
-
 
   useEffect(() => {
     props.setHeader("Main");
@@ -30,48 +31,45 @@ function Main(props) {
     getTotalPages();
   }, []);
 
-  
   useEffect(() => {
     setIsLoading(true);
     getCardsFromApi();
   }, [currentPage]);
 
   async function getCardsFromApi() {
-    setTimeout(async ()=>{
+    setTimeout(async () => {
       let cardsFromApi = await CardsService.getAll(currentPage);
       setCards(cardsFromApi);
       setIsLoading(false);
-    }, 1000)
-
+    }, 1000);
   }
 
   async function getTotalPages() {
-    setTimeout(async ()=>{
+    setTimeout(async () => {
       let totalPages = await CardsService.getTotalPages();
       setTotalPages(totalPages);
-    }, 1000)
-
+    }, 1000);
   }
 
- async function search(e){
-    if (e.keyCode === 13 || e === 'search'){  
+  async function search(e) {
+    if (e.keyCode === 13 || e === "search") {
       setIsLoading(true);
       setCards([]);
-      try{
+      try {
         let resultSearch = await CardsService.searchName(props.searchText);
         setCards(resultSearch);
         setIsLoading(false);
         setErrorLoading(false);
-      }catch{
+      } catch {
         setErrorLoading(true);
         setIsLoading(false);
-      }  
-    }else if(e === 'clear'){
+      }
+    } else if (e === "clear") {
       getCardsFromApi();
       setErrorLoading(false);
       setIsLoading(false);
     }
-  };
+  }
 
   async function handleOnclick(e) {
     let el = findParent(e, "card");
@@ -86,8 +84,23 @@ function Main(props) {
     setCards([...cards].sort((a, b) => a[sortName].localeCompare(b[sortName])));
   };
 
-  function handlerSetCurrentPage(numberPage){
+  function handlerSetCurrentPage(numberPage) {
     setCurrentPage(numberPage);
+  }
+
+  
+  function handlerSetNextPage() {
+    if((nextPage - 1)  < totalPages){
+      setNextPage(nextPage + 10);
+      setPrevPage(prevPage + 10);
+    }
+  }
+  
+  function handlerSetPrevPage() {
+    if(prevPage - 1  >= 1){
+      setNextPage(nextPage - 10);
+      setPrevPage(prevPage - 10);
+    }
   }
 
   return (
@@ -120,11 +133,20 @@ function Main(props) {
       </div>
 
       <div className="pages-wrapper">
-        <div>
-          {arrPages.map((item=>(
-          <span id = {item} onClick = {e=>(handlerSetCurrentPage(e.target.id))} className={item==currentPage ? "page page-current" : "page"}>{item}</span>)
+        <div className="pages-pagination">
+          <div className="page" onClick={handlerSetPrevPage}>&lt;</div>
+          {arrPages.filter(item => item > prevPage && item < nextPage)
+          .map((item) => (
+            <div
+              id={item}
+              onClick={(e) => handlerSetCurrentPage(e.target.id)}
+              className={item == currentPage ? "page page-current" : "page"}
+            >
+              {item}
+            </div>
           ))}
-          </div>
+          <div className="page" onClick={handlerSetNextPage}>&gt;</div>
+        </div>
       </div>
     </div>
   );
